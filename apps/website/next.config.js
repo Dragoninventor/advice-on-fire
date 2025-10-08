906f82033d52cd2cfb756d1bb4eb606710202eb6
michael <michael@dragoninventor.com>
2025-10-08 07:08:36 -0600

    Revert Dockerfile changes, update next.config.js

--- apps/website/next.config.js
+++ apps/website/next.config.js
@@ -1,9 +1,11 @@
 /** @type {import("next").NextConfig} */
 
 import { withPayload } from "@payloadcms/next/withPayload";
+import path from "path";
 
 const nextConfig = {
 	output: "standalone",
+	outputFileTracingRoot: path.join(__dirname, "../../"),
 	images: {
 		unoptimized: true,
 	},
